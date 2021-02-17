324240e0c8f96be7ec0cf08ebf054a9fbde4059a
stinefm <stinefmyhre@gmail.com>
2021-02-17 20:46:17 +0100

    added storage module

---
+++ stinetwork/results/storage.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+def save_history(comp_list:list, attribute:str, save_dir:str):
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+    df = pd.DataFrame()
+    for comp in comp_list:
+        data = comp.get_history(attribute)
+        df[comp] = data
+    df.to_csv(os.path.join(save_dir,attribute+".csv"))
+
+if __name__=="__main__":
+    pass
