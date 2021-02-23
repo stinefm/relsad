2f17f50ee5a4cd538fdba7a51750ffc764ab4986
stinefm <stinefmyhre@gmail.com>
2021-02-24 00:05:01 +0100

    added random_instance

--- stinetwork/utils.py
+++ stinetwork/utils.py
@@ -2,6 +2,8 @@
 This module contains package utils
 """
 
+import numpy as np
+
 def unique(sequence):
     """
     Return list of unique elements while preserving the order
@@ -20,6 +22,12 @@ def intersection(list1,list2):
     Returns the intersection between two list while preserving the order
     """
     return [x for x in list1 if x in list2]
+
+def random_instance(seed=None):
+    """
+    Return a numpy random instance with optional seed
+    """
+    return np.random.default_rng(seed)
 
 if __name__=="__main__":
     pass
