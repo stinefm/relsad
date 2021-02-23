ef406c459b0f40e34071bd9c4dfcfe1a498377e9
stinefm <stinefmyhre@gmail.com>
2021-02-23 23:24:38 +0100

    added utils

---
+++ stinetwork/utils.py
@@ -0,0 +1,25 @@
+"""
+This module contains package utils
+"""
+
+def unique(sequence):
+    """
+    Return list of unique elements while preserving the order
+    """
+    seen = set()
+    return [x for x in sequence if not (x in seen or seen.add(x))]
+
+def subtract(list1,list2):
+    """
+    Return difference between lists while preserving the order
+    """
+    return [x for x in list1 if x not in list2]
+
+def intersection(list1,list2):
+    """
+    Returns the intersection between two list while preserving the order
+    """
+    return [x for x in list1 if x in list2]
+
+if __name__=="__main__":
+    pass
