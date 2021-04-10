9e3695f66c5826b1e1c672624f89b0bae50a8a63
stinefm <stinefmyhre@gmail.com>
2021-04-10 12:50:02 +0200

    updated loads

--- stinetwork/test_networks/smallNetwork.py
+++ stinetwork/test_networks/smallNetwork.py
@@ -23,20 +23,19 @@ def initialize_test_network():
     fail_rate = 40
 
     ## Distribution network
-    B1 = Bus("B1", coordinate=[0, -1], fail_rate_per_year=fail_rate)  # 0.2)
-    B2 = Bus("B2", coordinate=[1, -2], fail_rate_per_year=fail_rate)  # 0.2)
-    B3 = Bus("B3", coordinate=[0, -2], fail_rate_per_year=fail_rate)  # 0.2)
-    B4 = Bus("B4", coordinate=[0, -3], fail_rate_per_year=fail_rate)  # 0.2)
-    B5 = Bus("B5", coordinate=[1, -3], fail_rate_per_year=fail_rate)  # 0.2)
+    B1 = Bus("B1", coordinate=[0, -1], fail_rate_per_year=fail_rate)
+    B2 = Bus("B2", coordinate=[1, -2], fail_rate_per_year=fail_rate)
+    B3 = Bus("B3", coordinate=[0, -2], fail_rate_per_year=fail_rate)
+    B4 = Bus("B4", coordinate=[0, -3], fail_rate_per_year=fail_rate)
+    B5 = Bus("B5", coordinate=[1, -3], fail_rate_per_year=fail_rate)
 
     ## Microgrid
-    M1 = Bus("M1", coordinate=[-1, -2], fail_rate_per_year=fail_rate)  # 0.2)
-    M2 = Bus("M2", coordinate=[-2, -3], fail_rate_per_year=fail_rate)  # 0.2)
-    M3 = Bus("M3", coordinate=[-1, -3], fail_rate_per_year=fail_rate)  # 0.2)
+    M1 = Bus("M1", coordinate=[-1, -2], fail_rate_per_year=fail_rate)
+    M2 = Bus("M2", coordinate=[-2, -3], fail_rate_per_year=fail_rate)
+    M3 = Bus("M3", coordinate=[-1, -3], fail_rate_per_year=fail_rate)
 
     Battery("Bat1", M1)
     Production("P1", M2)
-    # Production("P2",B5)
 
     L1 = Line(
         "L1",
@@ -45,7 +44,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
     L2 = Line(
         "L2",
         B1,
@@ -53,7 +52,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
     L3 = Line(
         "L3",
         B1,
@@ -61,7 +60,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
     L4 = Line(
         "L4",
         B3,
@@ -69,7 +68,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
     L5 = Line(
         "L5",
         B2,
@@ -77,7 +76,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
     L6 = Line(
         "L6",
         B3,
@@ -86,7 +85,7 @@ def initialize_test_network():
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
         capacity=6,
-    )  # 0.2)
+    )
     L7 = Line(
         "L7",
         B1,
@@ -94,7 +93,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
     ML1 = Line(
         "ML1",
         M1,
@@ -102,7 +101,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
     ML2 = Line(
         "ML2",
         M1,
@@ -110,7 +109,7 @@ def initialize_test_network():
         0.057526629463617,
         0.029324854498807,
         fail_rate_density_per_year=fail_rate,
-    )  # 0.2)
+    )
 
     E1 = CircuitBreaker("E1", L1)
     E2 = CircuitBreaker("E2", L7)
@@ -146,7 +145,7 @@ def initialize_test_network():
     dn.add_buses([B1, B2, B3, B4, B5])
     dn.add_lines([L2, L3, L4, L5, L6])
 
-    m = Microgrid(dn, L7)
+    m = Microgrid(dn, L7, mode=3)
 
     m.add_buses([M1, M2, M3])
     m.add_lines([ML1, ML2])
