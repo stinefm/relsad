9d517a888e6c74c6ab2095ec1c1f3861bc00ed40
stinefm <stinefmyhre@gmail.com>
2021-05-05 19:10:26 +0200

    made modified IEEE 69 bus system

---
+++ examples/bus69modified_monte_carlo.py
@@ -0,0 +1,230 @@
+from stinetwork.test_networks.test69modified import (
+    initialize_69Busmodified_network,
+)
+from stinetwork.visualization.plotting import plot_topology
+from stinetwork.utils import random_instance
+from load_and_gen_data import (
+    WeatherGen,
+    LoadGen,
+    windGen,
+    PVgeneration,
+)
+import time
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+start = time.time()
+
+ps = initialize_69Busmodified_network()
+
+## Set seed to get deterministic behavior
+ps.add_random_instance(random_instance(seed=0))
+
+if True:
+    # Fetching bus-objects
+    B1 = ps.get_comp("B1")
+    B2 = ps.get_comp("B2")
+    B3 = ps.get_comp("B3")
+    B4 = ps.get_comp("B4")
+    B5 = ps.get_comp("B5")
+    B6 = ps.get_comp("B6")
+    B7 = ps.get_comp("B7")
+    B8 = ps.get_comp("B8")
+    B9 = ps.get_comp("B9")
+    B10 = ps.get_comp("B10")
+    B11 = ps.get_comp("B11")
+    B12 = ps.get_comp("B12")
+    B13 = ps.get_comp("B13")
+    B14 = ps.get_comp("B14")
+    B15 = ps.get_comp("B15")
+    B16 = ps.get_comp("B16")
+
+    BM1 = ps.get_comp("BM1")
+    BM2 = ps.get_comp("BM2")
+    BM3 = ps.get_comp("BM3")
+    BM4 = ps.get_comp("BM4")
+
+if True:
+    # Fetching line-objects
+    L1 = ps.get_comp("L1")
+    L2 = ps.get_comp("L2")
+    L3 = ps.get_comp("L3")
+    L4 = ps.get_comp("L4")
+    L5 = ps.get_comp("L5")
+    L6 = ps.get_comp("L6")
+    L7 = ps.get_comp("L7")
+    L8 = ps.get_comp("L8")
+    L9 = ps.get_comp("L9")
+    L10 = ps.get_comp("L10")
+    L11 = ps.get_comp("L11")
+    L12 = ps.get_comp("L12")
+    L13 = ps.get_comp("L13")
+    L14 = ps.get_comp("L14")
+    L15 = ps.get_comp("L15")
+
+    ML1 = ps.get_comp("ML1")
+    ML2 = ps.get_comp("ML2")
+    ML3 = ps.get_comp("ML3")
+    ML4 = ps.get_comp("ML4")
+
+
+if True:
+    # Fetching disconnector objects
+    L1a = ps.get_comp("L1a")
+    L1b = ps.get_comp("L1b")
+    L1c = ps.get_comp("L1c")
+    L2a = ps.get_comp("L2a")
+    L2b = ps.get_comp("L2b")
+    L3a = ps.get_comp("L3a")
+    L3b = ps.get_comp("L3b")
+    L4a = ps.get_comp("L4a")
+    L4b = ps.get_comp("L4b")
+    L5a = ps.get_comp("L5a")
+    L5b = ps.get_comp("L5b")
+    L6a = ps.get_comp("L6a")
+    L6b = ps.get_comp("L6b")
+    L7a = ps.get_comp("L7a")
+    L7b = ps.get_comp("L7b")
+    L8a = ps.get_comp("L8a")
+    L8b = ps.get_comp("L8b")
+    L9a = ps.get_comp("L9a")
+    L9b = ps.get_comp("L9b")
+    L10a = ps.get_comp("L10a")
+    L10b = ps.get_comp("L10b")
+    L11a = ps.get_comp("L11a")
+    L11b = ps.get_comp("L11b")
+    L12a = ps.get_comp("L12a")
+    L12b = ps.get_comp("L12b")
+    L13a = ps.get_comp("L13a")
+    L13b = ps.get_comp("L13b")
+    L14a = ps.get_comp("L14a")
+    L14b = ps.get_comp("L14b")
+    L15a = ps.get_comp("L15a")
+    L15b = ps.get_comp("L15b")
+
+    ML1a = ps.get_comp("ML1a")
+    ML1b = ps.get_comp("ML1b")
+    ML1c = ps.get_comp("ML1c")
+    ML2a = ps.get_comp("ML2a")
+    ML2b = ps.get_comp("ML2b")
+    ML3a = ps.get_comp("ML3a")
+    ML3b = ps.get_comp("ML3b")
+    ML4a = ps.get_comp("ML4a")
+    ML4b = ps.get_comp("ML4b")
+
+
+# Fetching battery and production objects
+Bat1 = BM1.get_battery()
+P1 = BM3.get_production()
+P2 = BM4.get_production()
+
+temp_profiles, wind_profiles, solar_profiles = WeatherGen()
+
+wind = windGen(wind_profiles)
+PV = PVgeneration(temp_profiles, solar_profiles)
+
+(
+    load_house,
+    load_farm,
+    load_microgrid,
+    load_industry2,
+    load_trade,
+    load_office,
+) = LoadGen(temp_profiles)
+
+load_dict = dict()
+
+load_dict[B2] = {
+    "Husholdning": {"pload": load_house * 100, "qload": load_house * 0}
+}
+load_dict[B3] = {
+    "Offentlig virksomhet": {
+        "pload": load_office * 2,
+        "qload": load_office * 0,
+    }
+}
+load_dict[B4] = {
+    "Husholdning": {"pload": load_house * 90, "qload": load_house * 0}
+}
+load_dict[B5] = {
+    "Industri": {"pload": load_industry2 * 3, "qload": load_industry2 * 0}
+}
+load_dict[B6] = {
+    "Jordbruk": {"pload": load_farm * 100, "qload": load_farm * 0}
+}
+load_dict[B7] = {"Jordbruk": {"pload": load_farm * 90, "qload": load_farm * 0}}
+load_dict[B8] = {
+    "Offentlig virksomhet": {
+        "pload": load_office * 2,
+        "qload": load_office * 0,
+    }
+}
+load_dict[B9] = {
+    "Handel og tjenester": {
+        "pload": load_trade * 4,
+        "qload": load_trade * 0,
+    }
+}
+
+load_dict[B10] = {
+    "Industri": {"pload": load_industry2 * 2, "qload": load_industry2 * 0}
+}
+load_dict[B11] = {
+    "Jordbruk": {"pload": load_farm * 80, "qload": load_farm * 0}
+}
+load_dict[B12] = {
+    "Handel og tjenester": {
+        "pload": load_trade * 3,
+        "qload": load_trade * 0,
+    }
+}
+load_dict[B13] = {
+    "Husholdning": {"pload": load_house * 80, "qload": load_house * 0}
+}
+load_dict[B14] = {
+    "Husholdning": {"pload": load_house * 70, "qload": load_house * 0}
+}
+load_dict[B15] = {
+    "Jordbruk": {"pload": load_farm * 70, "qload": load_farm * 0}
+}
+
+load_dict[B16] = {
+    "Husholdning": {"pload": load_house * 60, "qload": load_house * 0}
+}
+
+
+# Microgrid:
+load_dict[BM2] = {
+    "Microgrid": {"pload": load_farm * 40, "qload": load_farm * 0}
+}
+
+prod_dict = dict()
+
+prod_dict[P1] = {"pprod": wind, "qprod": wind * 0}
+prod_dict[P2] = {"pprod": PV, "qprod": PV * 0}
+
+ps.add_load_dict(load_dict)
+ps.add_prod_dict(prod_dict)
+
+save_dir = r"C:\Users\stinefm\Documents\results69modified\S2"
+
+# fig = plot_topology(ps.buses, ps.lines, figsize=(6.5, 4.5))
+# fig.savefig(os.path.join(save_dir, "topology.pdf"))
+
+
+ps.run_monte_carlo(
+    iterations=10,
+    increments=8760,
+    save_iterations=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    save_dir=save_dir,
+)
+
+
+# print(ps.get_max_load())
+
+end = time.time()
+print("Time elapsed: {}".format(end - start))
+
+ps.plot_monte_carlo_history(os.path.join(save_dir, "monte_carlo"))
+ps.save_monte_carlo_history(os.path.join(save_dir, "monte_carlo"))
---
+++ stinetwork/test_networks/test69modified.py
@@ -0,0 +1,332 @@
+from stinetwork.network.components import (
+    Bus,
+    Line,
+    Disconnector,
+    CircuitBreaker,
+    Battery,
+    Production,
+)
+from stinetwork.network.systems import (
+    PowerSystem,
+    Transmission,
+    Distribution,
+    Microgrid,
+)
+from stinetwork.visualization.plotting import plot_topology
+
+
+def initialize_69Busmodified_network():
+    ps = PowerSystem()
+    fail_rate_trafo = 0.007  # 0.008
+    fail_rate_line = 0.07  # 0.08
+
+    B1 = Bus("B1", coordinate=[0, 0], fail_rate_per_year=0)
+    B2 = Bus("B2", coordinate=[1, 0], fail_rate_per_year=fail_rate_trafo)
+    B3 = Bus("B3", coordinate=[2, 0], fail_rate_per_year=fail_rate_trafo)
+    B4 = Bus("B4", coordinate=[3, 0], fail_rate_per_year=fail_rate_trafo)
+    B5 = Bus("B5", coordinate=[4, 0], fail_rate_per_year=fail_rate_trafo)
+    B6 = Bus("B6", coordinate=[5, 0], fail_rate_per_year=fail_rate_trafo)
+    B7 = Bus("B7", coordinate=[6, 0], fail_rate_per_year=fail_rate_trafo)
+    B8 = Bus("B8", coordinate=[7, 0], fail_rate_per_year=fail_rate_trafo)
+    B9 = Bus("B9", coordinate=[8, 0], fail_rate_per_year=fail_rate_trafo)
+    B10 = Bus("B10", coordinate=[9, 0], fail_rate_per_year=fail_rate_trafo)
+    B11 = Bus("B11", coordinate=[10, 0], fail_rate_per_year=fail_rate_trafo)
+    B12 = Bus("B12", coordinate=[11, 0], fail_rate_per_year=fail_rate_trafo)
+    B13 = Bus("B13", coordinate=[12, 0], fail_rate_per_year=fail_rate_trafo)
+    B14 = Bus("B14", coordinate=[13, 0], fail_rate_per_year=fail_rate_trafo)
+    B15 = Bus("B15", coordinate=[14, 0], fail_rate_per_year=fail_rate_trafo)
+    B16 = Bus("B16", coordinate=[15, 0], fail_rate_per_year=fail_rate_trafo)
+
+    # Microgrid:
+    BM1 = Bus("BM1", coordinate=[13, 1.5], fail_rate_per_year=fail_rate_trafo)
+    BM2 = Bus("BM2", coordinate=[14, 1.5], fail_rate_per_year=fail_rate_trafo)
+    BM3 = Bus("BM3", coordinate=[14, 2], fail_rate_per_year=fail_rate_trafo)
+    BM4 = Bus("BM4", coordinate=[14, 1], fail_rate_per_year=fail_rate_trafo)
+
+    Battery("Bat1", BM1)
+    Production("P1", BM3)
+    Production("P2", BM4)
+
+    # Lines, connections and impedances
+    L1 = Line(
+        "L1",
+        B1,
+        B2,
+        0.3660,
+        0.1864,
+        1.485093,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L2 = Line(
+        "L2",
+        B2,
+        B3,
+        0.3811,
+        0.1941,
+        1.437928,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L3 = Line(
+        "L3",
+        B3,
+        B4,
+        0.0922,
+        0.0470,
+        0.34788,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L4 = Line(
+        "L4",
+        B4,
+        B5,
+        0.0493,
+        0.0251,
+        0.186014,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L5 = Line(
+        "L5",
+        B5,
+        B6,
+        0.8190,
+        0.2707,
+        3.090168,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L6 = Line(
+        "L6",
+        B6,
+        B7,
+        0.1872,
+        0.0619,
+        0.706324,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L7 = Line(
+        "L7",
+        B7,
+        B8,
+        0.7114,
+        0.2351,
+        2.684183,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L8 = Line(
+        "L8",
+        B8,
+        B9,
+        1.0300,
+        0.3400,
+        3.886292,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L9 = Line(
+        "L9",
+        B9,
+        B10,
+        1.0440,
+        0.3450,
+        3.939116,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L10 = Line(
+        "L10",
+        B10,
+        B11,
+        1.0580,
+        0.3496,
+        3.991939,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L11 = Line(
+        "L11",
+        B11,
+        B12,
+        0.1966,
+        0.0650,
+        0.794615,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L12 = Line(
+        "L12",
+        B12,
+        B13,
+        1.0300,
+        0.3400,
+        3.886292,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L13 = Line(
+        "L13",
+        B13,
+        B14,
+        1.0440,
+        0.3450,
+        3.939116,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L14 = Line(
+        "L14",
+        B14,
+        B15,
+        1.0580,
+        0.3496,
+        3.991939,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    L15 = Line(
+        "L15",
+        B15,
+        B16,
+        0.1966,
+        0.0650,
+        0.794615,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+
+    # Microgrid
+
+    ML1 = Line(
+        "ML1",
+        B13,
+        BM1,
+        0.7394,
+        0.2444,
+        2.78983,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+
+    ML2 = Line(
+        "ML2",
+        BM1,
+        BM2,
+        0.0047,
+        0.0016,
+        0.017734,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    ML3 = Line(
+        "ML3",
+        BM1,
+        BM3,
+        0.0047,
+        0.0016,
+        0.017734,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+    ML4 = Line(
+        "ML4",
+        BM1,
+        BM4,
+        0.0047,
+        0.0016,
+        0.017734,
+        fail_rate_density_per_year=fail_rate_line,
+    )
+
+    E1 = CircuitBreaker("E1", L1)
+    E2 = CircuitBreaker("E2", ML1)
+
+    Disconnector("L1a", L1, B1, E1)
+    Disconnector("L1b", L1, B2, E1)
+    Disconnector("L1c", L1, B2)
+    Disconnector("L2a", L2, B2)
+    Disconnector("L2b", L2, B3)
+    Disconnector("L3a", L3, B3)
+    Disconnector("L3b", L3, B4)
+    Disconnector("L4a", L4, B4)
+    Disconnector("L4b", L4, B5)
+    Disconnector("L5a", L5, B5)
+    Disconnector("L5b", L5, B6)
+    Disconnector("L6a", L6, B6)
+    Disconnector("L6b", L6, B7)
+    Disconnector("L7a", L7, B7)
+    Disconnector("L7b", L7, B8)
+    Disconnector("L8a", L8, B8)
+    Disconnector("L8b", L8, B9)
+    Disconnector("L9a", L9, B9)
+    Disconnector("L9b", L9, B10)
+    Disconnector("L10a", L10, B10)
+    Disconnector("L10b", L10, B11)
+    Disconnector("L11a", L11, B11)
+    Disconnector("L11b", L11, B12)
+    Disconnector("L12a", L12, B12)
+    Disconnector("L12b", L12, B13)
+    Disconnector("L13a", L13, B13)
+    Disconnector("L13b", L13, B14)
+    Disconnector("L14a", L14, B14)
+    Disconnector("L14b", L14, B15)
+    Disconnector("L15a", L15, B15)
+    Disconnector("L15b", L15, B16)
+
+    # Microgrid:
+
+    Disconnector("ML1a", ML1, B13, E2)
+    Disconnector("ML1b", ML1, BM1, E2)
+    Disconnector("ML1c", ML1, BM1)
+    Disconnector("ML2a", ML2, BM1)
+    Disconnector("ML2b", ML2, BM2)
+    Disconnector("ML3a", ML3, BM1)
+    Disconnector("ML3b", ML3, BM3)
+    Disconnector("ML4a", ML4, BM1)
+    Disconnector("ML4b", ML4, BM4)
+
+    tn = Transmission(ps, B1)
+
+    dn = Distribution(tn, L1)
+
+    dn.add_buses(
+        [
+            B2,
+            B3,
+            B4,
+            B5,
+            B6,
+            B7,
+            B8,
+            B9,
+            B10,
+            B11,
+            B12,
+            B13,
+            B14,
+            B15,
+            B16,
+        ]
+    )
+
+    dn.add_lines(
+        [
+            L2,
+            L3,
+            L4,
+            L5,
+            L6,
+            L7,
+            L8,
+            L9,
+            L10,
+            L11,
+            L12,
+            L13,
+            L14,
+            L15,
+        ]
+    )
+
+    m = Microgrid(dn, ML1, mode=2)
+    m.add_buses([BM1, BM2, BM3, BM4])
+    m.add_lines([ML2, ML3, ML4])
+
+    return ps
+
+
+if __name__ == "__main__":
+    import os
+
+    # ps = initialize_69Busmodified_network()
+    # fig = plot_topology(ps.buses, ps.lines, figsize=(40, 40))
+
+    # fig.savefig(
+    #     os.path.join(os.path.dirname(os.path.abspath(__file__)), "test69modified.pdf")
+    # )
