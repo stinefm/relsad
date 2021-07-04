e4d5a23dee825608fe281ab2ab066b3d7c6134f1
Jonas Rudshaug <jonas.rudshaug@ntnu.no>
2021-07-04 13:13:06 +0200

    fixed wrong syntax

--- stinetwork/utils.py
+++ stinetwork/utils.py
@@ -41,11 +41,11 @@ def eq(x: float, y: float):
     return abs(x - y) < 1e-6
 
 def random_choice(
+    random_instance: np.random.Generator, 
+    p_true: float):
     """
     Returns random choice based on uniform probability distribution
     """
-    random_instance: np.random.Generator, 
-    p_true: float):
     return random_instance.random() < p_true
 
 
