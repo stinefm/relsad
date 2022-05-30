a0754927ba5fb5765383f6ba39fb09a1e554b2c8
stine <stine.f.myhre@ntnu.no>
2022-05-30 19:22:42 +0200

    updated README file type in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="relsad",  # Replace with your own username
+    name="relsad",
     version="0.0.1",
     author="Stine Fleischer Myhre",
     author_email="stine.f.myhre@ntnu.no",
-    description="A small example package",
+    description="A Python package for reliability assessment of modern distribution systems.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/stinefm/STNetwork",
